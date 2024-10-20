69459526b09479a7226e41a4057813b3845872c0
Inlet-back <mikeneko41@icloud.com>
2024-10-20 08:48:29 +0000

    edit修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [ :index, :show, :edit, :update]
   resources :books, only: [:new, :index, :show, :create, :edit, :update,:destroy]
   root to: 'homes#top'
-  get 'homes/about', to: "homes#about", as: 'about'
+  get 'home/about', to: "homes#about", as: 'about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
