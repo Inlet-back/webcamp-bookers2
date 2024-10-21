b890fd3c99ec948ac4f826816904309de7b9bd36
Inlet-back <mikeneko41@icloud.com>
2024-10-21 09:17:02 +0000

    user編集をコントローラー側で他人の制御

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,7 +35,10 @@ end
 
   def update
     @book = Book.find(params[:id])
-
+    user = @book.user
+  unless user.id == current_user.id
+    redirect_to books_path
+  end
     if @book.update(book_params)
        flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
